fa81cb4c8504df946f0ed189c35cfb757912ce62
Anushka Paswan <146178575+Anushkapaswan@users.noreply.github.com>
2026-07-30 11:04:01 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+public:
+    priority_queue<int>pq;
+    int kth=0;
+    KthLargest(int k, vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+        kth=k;
+    }
+    
+    int add(int val) {
+        int count=0;
+        int ans=0;
+        pq.push(val);
+        vector<int>popped;
+        for(int i=0;i<kth-1;i++){
+            popped.push_back(pq.top());
+            pq.pop();
+        }
+        ans=pq.top();
+        for(int i=0;i<popped.size();i++){
+            pq.push(popped[i]);
+        }
+        return ans;
+    }
+};
